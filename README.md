d213b7e561045d9bb04724f9d2b5ce7afb964311
Reece Williams <89943358+Tisreece@users.noreply.github.com>
2025-01-27 20:56:44 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BrickedUp
+
+Developed with Unreal Engine 5
---
+++ Source/BrickedUp/BrickedUp.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BrickedUp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BrickedUp, "BrickedUp" );
