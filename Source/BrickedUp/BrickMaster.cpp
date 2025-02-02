4ccb394108b0e5d9621f7a6c4cb994afdbe6ec6e
Reece Williams <89943358+Tisreece@users.noreply.github.com>
2025-02-02 21:13:28 +0000

    Brick
    - Added Brick Master

---
+++ Source/BrickedUp/BrickMaster.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BrickMaster.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ABrickMaster::ABrickMaster()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Scene Components
+	BrickCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Brick Collision"));
+	RootComponent = BrickCollision;
+	BrickCollision->SetBoxExtent(FVector(10.0f, 5.0f, 2.5f));
+	BrickCollision->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+
+	Brick = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Brick"));
+	Brick->SetupAttachment(BrickCollision);
+
+	//Bind Events
+	BrickCollision->OnComponentHit.AddDynamic(this, &ABrickMaster::OnHit);
+}
+
+// Called when the game starts or when spawned
+void ABrickMaster::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABrickMaster::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABrickMaster::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (OtherActor && OtherActor != this)
+	{
+		HitEffect();
+	}
+}
+
+// void ABrickMaster::HitEffect()
+// {
+
+// }
+
---
+++ Source/BrickedUp/BrickMaster.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BrickMaster.generated.h"
+
+UCLASS()
+class BRICKEDUP_API ABrickMaster : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABrickMaster();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Component") class UBoxComponent* BrickCollision;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Component") class UStaticMeshComponent* Brick;
+
+	//Functions
+	UFUNCTION(BlueprintCallable, Category="Hit") 
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	UFUNCTION(BlueprintImplementableEvent, Category="Hit") void HitEffect();
+
+};
