a7c5d37c72e007a58c1417680601c9ef47aa6584
Reece Williams <89943358+Tisreece@users.noreply.github.com>
2025-04-22 20:16:16 +0100

    Indestructible Brick added

---
+++ Source/BrickedUp/BrickIndestructible.cpp
@@ -0,0 +1,7 @@
+#include "BrickIndestructible.h"
+
+ABrickIndestructible::ABrickIndestructible()
+{
+    MaxHealth = -1;
+    Value = 10.0f;
+}
---
+++ Source/BrickedUp/BrickIndestructible.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BrickMaster.h"
+
+#include "BrickIndestructible.generated.h"
+
+UCLASS()
+class BRICKEDUP_API ABrickIndestructible : public ABrickMaster
+{
+    GENERATED_BODY()
+
+public:
+    ABrickIndestructible();
+};
