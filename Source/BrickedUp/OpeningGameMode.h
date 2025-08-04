01c704639e1426efe67342b624465559d531a24d
Reece Williams <89943358+Tisreece@users.noreply.github.com>
2025-08-04 20:23:45 +0100

    Opening screen

---
+++ Source/BrickedUp/OpeningGameMode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpeningGameMode.generated.h"
+
+UCLASS()
+class BRICKEDUP_API AOpeningGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+};
